bf73742fc06e8a0926ddab0573cc2f8c9954b3eb
luishurtado14x-codificador <luis.hurtado14.x@gmail.com>
2026-04-17 14:53:27 -0400

    agregamos el directorio

---
+++ wetetwy/wetetwy/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: usuario
+ * Fecha: 17/4/2026
+ * Hora: 2:32 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.IO;
+
+namespace wetetwy
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+		//directorio
+		
+		string rutaRaiz = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"Datos_iujo");
+		string rutaReportes =  Path.Combine(rutaRaiz, "Reportes");
+		Console.Write(rutaReportes);
+		
+		if (!Directory.Exists(rutaReportes)){
+			
+			Directory.CreateDirectory(rutaReportes);
+			
+			Console.WriteLine(" \n directorio creado");
+					
+		}
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
